c74e404f2da4825575941c1e246470542982df39
jeffnorville <jeff@jnorville.com>
2016-06-27 13:39:13 +0200

    initialization project

---
+++ ProgRichards/src/ProgRichards.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : ProgRichards.cpp
+// Author      : jn
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Dave...?" << endl; // prints Dave...
+	return 0;
+}
+
+
+
